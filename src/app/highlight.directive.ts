5e1ffc116467b28e2b4317076c95f3c736cbfd8c
haidepzai <sv048@hdm-stuttgart.de>
2020-10-24 12:27:02 +0200

    2 files modified

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,34 +1,36 @@
 /* tslint:disable:member-ordering */
-import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from "@angular/core";
 
 @Directive({
-  selector: '[appHighlight]'
+  selector: "[appHighlight]"
 })
 export class HighlightDirective {
-
-  constructor(private el: ElementRef) { }
+  //You use the ElementRef in the directive's constructor to inject a reference to the host DOM element, the element to which you applied appHighlight.
+  constructor(private el: ElementRef) {}
 
   @Input() defaultColor: string;
 
-  @Input('appHighlight') highlightColor: string;
+  @Input("appHighlight") highlightColor: string;
+  //appHighlight bezieht sich auf Directive selector [appHighlight]
+
+  @Input() testHighlighter: string;
 
   //Beim Hovern (MouseEnter)
   //Methode hightlight() wird aufgerufen
   //Parameter: Entweder ausgewählte HighLightColor oder DefaultColor (violet) oder einfach nur rot
-  @HostListener('mouseenter') onMouseEnter() {
-    this.highlight(this.highlightColor || this.defaultColor || 'red');
+  @HostListener("mouseenter") onMouseEnter() {
+    this.highlight(this.highlightColor || this.defaultColor || this.testHighlighter || "red");
   }
 
-  @HostListener('mouseleave') onMouseLeave() {
+  @HostListener("mouseleave") onMouseLeave() {
     this.highlight(null);
   }
 
-  //Ändert Hintergrundfarbe
+  //Ändert Hintergrundfarbe vom Element
   private highlight(color: string) {
     this.el.nativeElement.style.backgroundColor = color;
   }
 }
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
